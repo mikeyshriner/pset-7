740d125d2d682e0e4a9f19877b4c80186c2d3a89
mikeyshriner <shrinermichael@gmail.com>
2020-01-20 21:11:57 -0500

    more corrections

--- src/exercises.js
+++ src/exercises.js
@@ -18,63 +18,64 @@ function commonEnd(a, b) {
 }
 
 function endsMeet(values, n) {
-  let empty = [];
+  let array = []
 
-  if (!values || values.length < n || n <= 0) {
-    return empty;
-  }
-  else if (n > 0) {
-    let newArray = [];
-    let first;
-
-    for (f = 0; f <= n - 1; f++) {
-      first = values[f];
-      newArray.push(first);
-    }
-    let end = values.length - 1;
-    let endStart = end - n;
-    for (e = endStart; e <= end; e++) {
-      newArray.push(e);
-    }
-    return newArray;
+  if (!values || values.length < n || !Number.isInteger(n) || n < 0) {
+    return array;
   } else {
-    return values;
+      if (n === 0) {
+        return values;
+      }
+
+      for (let i = 0; i < n; i++) {
+        if (values[i] !== undefined) {
+          array.push(values[i])
+        }
+      }
+
+      for (let i = n; i > 0; i--) {
+        if (values[values.length - i] !== undefined) {
+          array.push(values[values.length - i])
+        }
+      }
+      return array;
   }
  }
-
- return returnMe;
-}
 
 function difference(numbers) {
-  if (!numbers || numbers.length < 1 || !numbers.every(isNumber)) {
-   return undefined;
- } else {
-   const min = Math.min(...numbers);
-   const max = Math.max(...numbers);
+  if (numbers == undefined || numbers < 1 || numbers.some(isNaN)) {
+    return undefined;
+  } else {
 
-   return max - min;
- }
+    let bigInteger = Math.max.apply(null, numbers)
+    let smallInteger = Math.min.apply(null, numbers)
+
+    let difference = bigInteger - smallInteger
+
+    return difference
+  }
 }
 
 function max(number) {
-  if (!number || number.length === 0) {
-   return undefined;
- }
- if (number.length % 2 === 0 || number.length < 3 ){
-   return undefined;
- }
- if (number.some(isNaN)){
-   return undefined;
- }
- else{
-   var first_element = number[0];
-   var middle_element = number[Math.floor(number.length/2)];
-   var last_element = number[number.length-1];
-   var array = [];
-   var largest = Number(Math.max.apply(null, array));
-   array.push(first_element, middle_element, last_element);
-   return  Number(Math.max.apply(null, array));
- }
+  if (number == undefined || number <= 2 || number.some(isNaN)) {
+
+  return undefined;
+
+} else if (number.length % 2 == 0) {
+
+  return undefined;
+
+} else {
+   let lastElement = number[number.length - 1]
+   let middleElement = number[number.length - (Math.ceil(number.length / 2))]
+   let firstElement = number[0]
+
+   let array = [firstElement, middleElement, lastElement]
+
+   let largestValue = Math.max.apply(null, array)
+
+   return largestValue;
+}
 }
 
 function middle(values) {
